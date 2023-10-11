e0b3ec284bb482e99a8c7bdc9a4d971d494dd867
DESKTOP-HBPJULN\User <teteocomputer@gmail.com>
2023-10-11 17:52:29 +0900

    4 15번 문제 편지 풀이

---
+++ programmers/15편지.js
@@ -0,0 +1,33 @@
+let text= "happy birthday!";
+let sum =0;
+function solution(message) {
+    console.log(message.length*2);
+    return message.length*2;
+}
+
+solution(text)
+
+
+// ================================================
+// let text = "happy birthday!"
+// let text2 ="I love you~"
+// let sum = 0;
+// let sum2 =0;
+
+// function solution(message) {
+//     let sum = text.length *2;
+//     let sum2 = text2.length *2;
+//     let answer = [sum, sum2]
+//     console.log(answer)
+//     return answer;
+// }
+
+// solution(text)
+
+// ================================================
+// let text = "happy birthday!"
+// let sum=0;
+// for(let i=0; i< text.length; i++){
+//        sum += 1;
+// }
+// console.log(sum*2);
